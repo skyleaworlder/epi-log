f5b85aa69c9d5da50057f60f1612bebfc695cc07
skyleaworlder <870033938@qq.com>
2021-07-22 17:13:03 +0800

    M: change log level

--- config.go
+++ config.go
@@ -3,12 +3,14 @@ package epilog
 type logLevel int
 
 const (
+	// NORMAL is a type
+	NORMAL logLevel = 0
 	// DEBUG is a type
-	DEBUG logLevel = 0
+	DEBUG logLevel = 1
 	// INFO is a type
-	INFO logLevel = 1
+	INFO logLevel = 2
 	// WARNING is a type
-	WARNING logLevel = 2
+	WARNING logLevel = 3
 	// FATAL is a type
-	FATAL logLevel = 3
+	FATAL logLevel = 4
 )
