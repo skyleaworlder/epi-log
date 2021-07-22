273a1cc1a2205ab127d77b329ed377340092a8eb
skyleaworlder <870033938@qq.com>
2021-07-22 22:01:25 +0800

    M: add exported function

--- export.go
+++ export.go
@@ -1,10 +1,50 @@
 package epilog
 
 var (
-//stdio *Logger = New()
+	stdio *Logger = New()
 )
 
-// Println logs a message at level info on the std logger
-func Println(args ...interface{}) {
-	// TODO
+// Use is an exported function.
+func Use() {
+	stdio.Use()
+}
+
+// End is an exported function.
+func End() {
+	stdio.End()
+}
+
+// ChangeLevel is an exported function.
+func ChangeLevel(level logLevel) {
+	stdio.ChangeLevel(level)
+}
+
+// RegisterAppender is an exported function.
+func RegisterAppender(name string, appender Appender) (err error) {
+	return stdio.RegisterAppender(name, appender)
+}
+
+// Print is an exported function.
+func Print(args ...interface{}) {
+	stdio.Print(args...)
+}
+
+// Debugln is an exported function.
+func Debugln(args ...interface{}) {
+	stdio.Debugln(args...)
+}
+
+// Infoln is an exported function.
+func Infoln(args ...interface{}) {
+	stdio.Infoln(args...)
+}
+
+// Warningln is an exported function.
+func Warningln(args ...interface{}) {
+	stdio.Warningln(args...)
+}
+
+// Fatalln is an exported function.
+func Fatalln(args ...interface{}) {
+	stdio.Fatalln(args...)
 }
