2d1ecc8ea5b87403ea427b2e745e56a2da4fdd1e
skyleaworlder <870033938@qq.com>
2021-07-22 17:16:01 +0800

    M: Preliminary finish log.go structure

--- log.go
+++ log.go
@@ -5,31 +5,36 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"runtime"
 	"sync"
 	"syscall"
-	"time"
 )
 
 // Logger is a struct
 type Logger struct {
+	// FATAL > WARNING > INFO > DEBUG
 	level logLevel
 
+	// map is much better than slice!
+	// do not try to persuade me.
 	appenders map[string]Appender
 	buffer    *Buffer
 
-	cnt int
+	// seqNum(sequence number) is logItem id
+	seqNum int
 
 	mtx *sync.Mutex
 }
 
 // New is a constructor
 func New(level logLevel, maxItemNum int) (lgr *Logger) {
+	appenders := make(map[string]Appender)
+	appenders["stdio"] = &StdAppender{}
 	return &Logger{
-		level:  level,
-		buffer: NewBuffer(maxItemNum),
-		cnt:    0,
-		mtx:    &sync.Mutex{},
+		level:     level,
+		appenders: appenders,
+		buffer:    NewBuffer(maxItemNum),
+		seqNum:    0,
+		mtx:       &sync.Mutex{},
 	}
 }
 
@@ -46,13 +51,13 @@ func (lgr *Logger) Run() {
 			switch sig {
 			case syscall.SIGINT:
 				fmt.Println("syscall.SIGINT:", sig)
-				lgr.UrgentExit()
+				lgr.urgentExit()
 			case syscall.SIGTERM:
 				fmt.Println("syscall.SIGTERM:", sig)
-				lgr.UrgentExit()
+				lgr.urgentExit()
 			case syscall.SIGKILL:
 				fmt.Println("syscall.SIGKILL:", sig)
-				lgr.UrgentExit()
+				lgr.urgentExit()
 			default:
 				fmt.Println("what?")
 			}
@@ -62,8 +67,35 @@ func (lgr *Logger) Run() {
 	lgr.Monitor()
 }
 
-// UrgentExit is to exit
-func (lgr *Logger) UrgentExit() {
+// urgentExit is to exit
+func (lgr *Logger) urgentExit() {
+	for item := range lgr.buffer.items {
+		// append content to appenders
+		for _, appender := range lgr.appenders {
+			err := appender.Append(item.Serialize())
+			if err != nil {
+				// in urgentExit, "Append Failure" is a fatal error
+				os.Exit(0)
+				goto AppendError
+			}
+		}
+		if len(lgr.buffer.items) == 0 {
+			// should not use "lgr.buffer.empty <- true" here.
+			// this statement is used in Monitor method.
+			// so "lgr.buffer.empty <- true" would be blocked here.
+			// and break statement would not be executed at once.
+
+			// it needs to break this for-range at once.
+			break
+		}
+	}
+	os.Exit(0)
+
+AppendError:
+	errmsg := "epilog.urgentExit Error: Appender Append Failed"
+	if err := lgr.appenders["stdio"].Append(errmsg); err != nil {
+		fmt.Println("epilog.urgentExit Error: Appender stdio Append Failed")
+	}
 	os.Exit(0)
 }
 
@@ -71,11 +103,23 @@ func (lgr *Logger) UrgentExit() {
 func (lgr *Logger) Monitor() {
 	for {
 		select {
+		// if buffer is full, then process bufferItem in buffer.
+		// flush the whole buffer until len(buffer.items) == 0.
 		case _ = <-lgr.buffer.full:
 			lgr.buffer.mtx.Lock()
+			// use for-range to receive bufferItem.
 			for item := range lgr.buffer.items {
-				// TODO: send/write items here!!!
-				fmt.Println(item.Serialize())
+				// Append of each appender should be called.
+				// if err occur, ignore it and move on.
+				for _, appender := range lgr.appenders {
+					err := appender.Append(item.Serialize())
+					if err != nil {
+						errmsg := "epilog.Monitor Error: Appender Append Failed:"
+						lgr.appenders["stdio"].Append(errmsg + err.Error())
+					}
+				}
+
+				// if len(buffer.items) equal 0, then break for-range.
 				if len(lgr.buffer.items) == 0 {
 					lgr.buffer.empty <- true
 					break
@@ -107,17 +151,36 @@ func (lgr *Logger) RegisterAppender(name string, appender Appender) (err error) 
 
 // Print is to print
 func (lgr *Logger) Print(args ...interface{}) {
-	// Print ignore Logger.level
-	fmt.Print(args...)
-	content := fmt.Sprint(args...)
-	_, filename, line, _ := runtime.Caller(0)
+	innerPrint(lgr, false, NORMAL, args...)
+	lgr.seqNum++
+}
 
-	// put into buffer
-	lgr.buffer.Put(BufferItem{
-		id: lgr.cnt, time: time.Now(),
-		filename: filename, line: line,
-		content: content,
-	})
+// Debugln is to debugln
+func (lgr *Logger) Debugln(args ...interface{}) {
+	if lgr.level <= DEBUG {
+		innerPrint(lgr, true, DEBUG, args...)
+		lgr.seqNum++
+	}
+}
 
-	lgr.cnt++
+// Infoln is to infoln
+func (lgr *Logger) Infoln(args ...interface{}) {
+	if lgr.level <= INFO {
+		innerPrint(lgr, true, INFO, args...)
+		lgr.seqNum++
+	}
+}
+
+// Warningln is to warningln
+func (lgr *Logger) Warningln(args ...interface{}) {
+	if lgr.level <= WARNING {
+		innerPrint(lgr, true, WARNING, args...)
+		lgr.seqNum++
+	}
+}
+
+// Fatalln is to fatalln
+func (lgr *Logger) Fatalln(args ...interface{}) {
+	innerPrint(lgr, true, FATAL, args...)
+	os.Exit(0)
 }
