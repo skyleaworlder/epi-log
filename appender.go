da3b16ed5b773872158eda2362a7bf6c5eac9b62
skyleaworlder <870033938@qq.com>
2021-07-22 18:48:56 +0800

    M: change FileAppender struct property: remove fd, add filepath & add defer fd.Close()

--- appender.go
+++ appender.go
@@ -31,26 +31,35 @@ func (std *StdAppender) Append(content string) (err error) {
 // FileAppender is an appender.
 // name is the key of appenders map.
 type FileAppender struct {
-	id   int
-	name string
-	fd   *os.File
+	id       int
+	name     string
+	filepath string
 }
 
 // NewFileAppender is a constructor of FileAppender.
-func NewFileAppender(name string, fd *os.File) *FileAppender {
+func NewFileAppender(name string, filepath string) *FileAppender {
 	defer func() { appenderID++ }()
 	return &FileAppender{
-		id:   appenderID,
-		name: name,
-		fd:   fd,
+		id:       appenderID,
+		name:     name,
+		filepath: filepath,
 	}
 }
 
 // Append is to implement interface "Appender"
 func (fa *FileAppender) Append(content string) (err error) {
 	buf := []byte(content)
-	if n, err := fa.fd.Write(buf); n < len(content) || err != nil {
-		errmsg := "epilog.Append error: FileAppender Write Failed"
+
+	// create file descriptor
+	fd, err := os.OpenFile(fa.filepath, os.O_CREATE|os.O_APPEND, 0222)
+	if err != nil {
+		errmsg := "epilog.Append error: FileAppender Create fd Failed:"
+		return errors.New(errmsg + err.Error())
+	}
+	defer fd.Close()
+
+	if n, err := fd.Write(buf); n < len(content) || err != nil {
+		errmsg := "epilog.Append error: FileAppender Write Failed:"
 		return errors.New(errmsg + err.Error())
 	}
 	return
@@ -81,6 +90,7 @@ func (sa *SocketAppender) Append(content string) (err error) {
 		errmsg := "epilog.Append error: SocketAppender Connect Failed"
 		return errors.New(errmsg + err.Error())
 	}
+	defer conn.Close()
 
 	buf := []byte(content)
 	if n, err := conn.Write(buf); n < len(content) || err != nil {
