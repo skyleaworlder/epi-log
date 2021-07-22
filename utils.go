2f22975591f2dc3996e03a624268db4420af434b
skyleaworlder <870033938@qq.com>
2021-07-22 17:14:46 +0800

    A: add utils, use innerPrint tool function, use logger method as wrapper

---
+++ utils.go
@@ -0,0 +1,38 @@
+package epilog
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func innerPrint(lgr *Logger, newLine bool, level logLevel, args ...interface{}) {
+	var content string
+	if newLine {
+		content = fmt.Sprintln(args...)
+	} else {
+		content = fmt.Sprint(args...)
+	}
+	_, filename, line, _ := runtime.Caller(0)
+
+	var typ string
+	switch level {
+	case NORMAL:
+		typ = "NORMAL"
+	case DEBUG:
+		typ = "DEBUG"
+	case INFO:
+		typ = "INFO"
+	case WARNING:
+		typ = "WARNING"
+	case FATAL:
+		typ = "FATAL"
+	default:
+		typ = "NORMAL"
+	}
+	lgr.buffer.Put(BufferItem{
+		id: lgr.seqNum, typ: typ,
+		filename: filename, line: line,
+		time: time.Now(), content: content,
+	})
+}
